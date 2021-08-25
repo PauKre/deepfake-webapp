cd36ae8ebb815c0244fb3b0ce2bfee86a4c6b1b9
aj52izov <jovial.t.silatsa@fau.de>
2021-08-25 13:26:32 +0200

    testen dein wissen first commit

--- frontend/views/testedeinwissen/testedein-wissen-view.ts
+++ frontend/views/testedeinwissen/testedein-wissen-view.ts
@@ -1,6 +1,9 @@
 import { html, LitElement, customElement } from 'lit-element';
-
-
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-select/src/vaadin-select.js';
+import '@vaadin/vaadin-list-box/src/vaadin-list-box.js';
+import '@vaadin/vaadin-item/src/vaadin-item.js';
 
 @customElement('testedein-wissen-view')
 export class TestedeinWissenView extends LitElement {
@@ -9,6 +12,52 @@ export class TestedeinWissenView extends LitElement {
     return this;
   }
   render() {
-    return html`<div>Content placeholder</div>`;
+    return html`
+<vaadin-horizontal-layout class="content" style=" background-color:white; width: 100%; height: 100%;">
+ <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout" style="width: 15%;"></vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout1" style="width: 70%;">
+<div >
+  <h2 responsive plain-text style="color:#B02E0C; margin: var(--lumo-space-m); flex-shrink: 1;">Werde zum Deepfake Experten!
+</h2>
+<h3 responsive plain-text style="color:#B02E0C; margin: var(--lumo-space-m); flex-shrink: 1;">
+Du bist schon einen Schritt näher, um dem Waschbär zu entlarven. </br> 
+Beantworte die Fragen der Meisterin Foxi, um dein Wissen zu Testen.
+</h3>
+</div>
+  <vaadin-horizontal-layout spacing="false" responsive :middle :center id="vaadinHorizontalLayout1">
+   <img id="catImgId" src="images/testedeinwissen/squirrel.png" responsive style=" width: 50%; height: 60%;">
+   <vaadin-vertical-layout spacing="false" responsive size-auto margin="false" :right>
+    <vaadin-label plain-text :center>
+      Label 
+    </vaadin-label>
+    <vaadin-select value="Item one">
+     <template>
+      <vaadin-list-box selected="0">
+       <vaadin-item selected>
+         Item one 
+       </vaadin-item>
+       <vaadin-item>
+         Item two 
+       </vaadin-item>
+       <vaadin-item>
+         Item three 
+       </vaadin-item>
+      </vaadin-list-box>
+     </template>
+    </vaadin-select>
+    <vaadin-horizontal-layout>
+     <vaadin-button responsive id="checkResultBttnId" plain-text>
+       Check Result 
+     </vaadin-button>
+     <vaadin-button responsive id="nextQuestBttnId" plain-text :right>
+       Next 
+     </vaadin-button>
+    </vaadin-horizontal-layout>
+   </vaadin-vertical-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="width: 15%;"></vaadin-vertical-layout>
+</vaadin-horizontal-layout>
+`;
   }
 }
--- frontend/views/wirbrauchendeinehilfe/wirbrauchendeine-hilfe-view.ts
+++ frontend/views/wirbrauchendeinehilfe/wirbrauchendeine-hilfe-view.ts
@@ -1,6 +1,8 @@
 import { html, LitElement, customElement } from 'lit-element';
 import '@vaadin/vaadin-button/src/vaadin-button.js';
 import '@polymer/iron-icon/iron-icon.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 
 @customElement('wirbrauchendeine-hilfe-view')
 export class WirbrauchendeineHilfeView extends LitElement {
@@ -10,24 +12,29 @@ export class WirbrauchendeineHilfeView extends LitElement {
   }
   render() {
   return html`
-<vaadin-horizontal-layout theme="spacing" style="background-color:white; width: 100%; height: 100%;" id="vaadinHorizontalLayout">
- <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout" style="width: 25%; height: 25%;"></vaadin-vertical-layout>
- <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout1" style="width: 50%; height: 50%;">
-  <vaadin-horizontal-layout spacing="false" responsive :middle :center id="vaadinHorizontalLayout1">
-   <img id="catImgId" src="images/wirbrauchendeinehilfe/cat.png" responsive style="width: 50%; height: 50%;">
-   <img id="porkImgId" src="images/wirbrauchendeinehilfe/pork.png" responsive style="width: 50%; height: 50%;">
+<vaadin-horizontal-layout class="content" style="background-color:white; width: 100%; height: 100%;">
+ <vaadin-vertical-layout theme="spacing" style="width: 25%;"></vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="width: 100%; height: 100%;">
+<div >
+  <h2 responsive plain-text style="color:#B02E0C; margin: var(--lumo-space-m); flex-shrink: 1;">Werde zum Deepfake Experten!
+</h2>
+</div>
+  <vaadin-horizontal-layout theme="spacing">
+   <img id="catImgId" src="images/wirbrauchendeinehilfe/cat.png" src="images/wirbrauchendeinehilfe/cat.png" responsive style="width: 50%; height: 50%;">
+   <img id="porkImgId" src="images/wirbrauchendeinehilfe/pork.png"  responsive style="width: 50%; height: 50%;" >
   </vaadin-horizontal-layout>
-  <vaadin-horizontal-layout spacing="false" responsive :middle :center id="vaadinHorizontalLayout2" style="width: 100%; height: 100%;">
+<vaadin-horizontal-layout spacing="false" responsive :middle :center id="vaadinHorizontalLayout2" style="width: 100%; height: 100%;">
    <vaadin-vertical-layout theme="spacing" id="vVerticalLayout1" style="width: 25%; height: 25%;"></vaadin-vertical-layout>
    <vaadin-vertical-layout theme="spacing" id="bVerticalLayout12" style="width: 50%; height: 50%;">
-    <vaadin-button id="startButtonId" onClick="location.href='learn'" icon="" responsive plain-text :middle :center style="color:white ;background-color:#B02E0C; width: 100%; height: 100%;">
+    <vaadin-button id="startButtonId" onclick="location.href='learn'" icon="" responsive plain-text :middle :center style="color:white ;background-color:#B02E0C; width: 100%; height: 100%;">
      <iron-icon icon="lumo:arrow-right" slot="suffix" id="ironIcon" style="color:white;"></iron-icon> Start 
     </vaadin-button>
    </vaadin-vertical-layout>
    <vaadin-vertical-layout theme="spacing" id="vVerticalLayout12" style="width: 25%; height: 25%;"></vaadin-vertical-layout>
   </vaadin-horizontal-layout>
+
  </vaadin-vertical-layout>
- <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout2" style="width: 25%; height: 25%;"></vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="width: 25%;"></vaadin-vertical-layout>
 </vaadin-horizontal-layout>
 `;
   }

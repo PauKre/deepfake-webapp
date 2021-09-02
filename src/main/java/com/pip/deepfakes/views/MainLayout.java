74b9b77d1af541ec4fff26479982dfad1f227248
diegiesskanne <jaromir.vogt@web.de>
2021-09-02 16:38:55 +0200

    first progress bar draft works

--- src/main/java/com/pip/deepfakes/views/MainLayout.java
+++ src/main/java/com/pip/deepfakes/views/MainLayout.java
@@ -1,20 +1,25 @@
 package com.pip.deepfakes.views;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.ComponentUtil;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.NativeButton;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.progressbar.ProgressBar;
+import com.vaadin.flow.component.progressbar.ProgressBarVariant;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.tabs.TabsVariant;
@@ -38,6 +43,14 @@ import com.vaadin.flow.component.avatar.Avatar;
 @Theme(themeFolder = "deepfakewebapp", variant = Lumo.DARK)
 @PageTitle("Main")
 public class MainLayout extends AppLayout {
+
+    public static ProgressBar learnprogress = new ProgressBar(0.0, 6.0, 0.0);
+
+    public static final Tabs tabs = new Tabs();
+
+    public static Boolean[] clickedTabs = new Boolean[6];
+
+    public static ComponentEventListener<Tabs.SelectedChangeEvent> listener;
 
     public static class MenuItemInfo {
 
@@ -77,10 +90,10 @@ public class MainLayout extends AppLayout {
         VerticalLayout layout = new VerticalLayout();
         layout.getThemeList().add("dark");
         layout.setWidthFull();
-        layout.setSpacing(false);
-        layout.setPadding(false);
+        layout.setSpacing(true);
+        layout.setPadding(true);
         layout.setAlignItems(FlexComponent.Alignment.CENTER);
-        layout.add(header, menu);
+        layout.add(header, menu, learnprogress);
         return layout;
     }
 
@@ -104,12 +117,25 @@ public class MainLayout extends AppLayout {
     }
 
     private Tabs createMenuTabs() {
-        final Tabs tabs = new Tabs();
+        learnprogress.addThemeVariants(ProgressBarVariant.LUMO_SUCCESS);
+        Arrays.fill(clickedTabs, Boolean.FALSE);
+        createListener();
+        tabs.addSelectedChangeListener(listener);
         tabs.getStyle().set("max-width", "100%");
         for (Tab menuTab : createMenuItems()) {
             tabs.add(menuTab);
         }
         return tabs;
+    }
+
+    private void createListener(){
+        listener = selectedChangeEvent -> {
+            double value = learnprogress.getValue() + 1;
+            if (value <= learnprogress.getMax() && !clickedTabs[tabs.getSelectedIndex()]) {
+                clickedTabs[tabs.getSelectedIndex()] = true;
+                learnprogress.setValue(value);
+            }
+        };
     }
 
     private List<Tab> createMenuItems() {

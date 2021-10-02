ed3c4a442ea8466109ed35a7d460d44c2fd4f9e6
PauKre <paulkremer@mail.de>
2021-10-02 12:07:21 +0200

    ACHTUNG, Seite laedt ewig

--- frontend/views/erkennediedeepfakes/erkennedie-deepfakes-view.ts
+++ frontend/views/erkennediedeepfakes/erkennedie-deepfakes-view.ts
@@ -17,9 +17,9 @@ export class ErkennedieDeepfakesView extends LitElement {
 <vaadin-horizontal-layout class="content" style="width: 100%; align-items: flex-start; justify-content: center; background-color:white; height: 100%;">
  <vaadin-vertical-layout theme="spacing" style="align-self: flex-start; align-items: center; justify-content: flex-start; width: 5%;"></vaadin-vertical-layout>
  <vaadin-vertical-layout style="align-self: center; align-items: center; justify-content: center; width: 70%; height: 100%; padding: var(--lumo-space-m);">
-  <h2 style="color:#B02E0C; align-self: center; height: 3%;">Werde zum Deepfake Experten!</h2>
-  <h3 style="color:#B02E0C; align-self: center; flex-grow: 0; height: 6%;">Trainiere deine Fähigkeit Deepfakes zu erkennen!</h3>
-  <img id="imageId" style="width: 60%; height: 60%; align-self: center;">
+  <h2>Werde zum Deepfake Experten!</h2>
+  <h3>Trainiere deine F&auml;higkeit Deepfakes zu erkennen!</h3>
+  <img id="imageId" style="width: 50%; height: 50%; align-self: center;">
   <vaadin-horizontal-layout theme="spacing" style="flex-grow: 1; flex-shrink: 1; width: 100%; justify-content: center; height: 10%;">
    <vaadin-button id="prevImgBttnId" >
      Vorherige Frage 

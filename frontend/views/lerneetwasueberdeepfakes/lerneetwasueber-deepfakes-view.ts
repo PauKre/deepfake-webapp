ae8c8fdf0f80affcd79e17992a3c970185840147
PauKre <paulkremer@mail.de>
2021-10-02 13:32:03 +0200

    Button hinzugefuegt

--- frontend/views/lerneetwasueberdeepfakes/lerneetwasueber-deepfakes-view.ts
+++ frontend/views/lerneetwasueberdeepfakes/lerneetwasueber-deepfakes-view.ts
@@ -10,6 +10,16 @@ export class LerneetwasueberDeepfakesView extends LitElement {
   }
   render() {
 
-    return html`<iframe id="prezi" src="https://prezi.com/view/0JLhl8gpxRfqSZFixkFN/embed?&amp;wmode=opaque" width="900" height="600" allowfullscreen="allowfullscreen"></iframe>`;
+    return html`
+      <vaadin-horizontal-layout class="content" style="width: 100%; justify-content: center; align-items: flex-start; background-color:white;">
+      <vaadin-vertical-layout theme="spacing" style="width: 50%; margin: var(--lumo-space-m); align-self: flex-start; justify-content: flex-start; align-items: center;">
+      <iframe id="prezi" src="https://prezi.com/view/0JLhl8gpxRfqSZFixkFN/embed?&amp;wmode=opaque" width="900" height="600" allowfullscreen="allowfullscreen"></iframe>
+          <vaadin-button theme="icon" id="ContinueId">
+              Weiter
+              <iron-icon icon="lumo:arrow-right"></iron-icon>
+          </vaadin-button>
+      </vaadin-vertical-layout>
+      </vaadin-horizontal-layout>
+      `;
   }
 }
